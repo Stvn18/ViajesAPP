3b9412001e6a3e47db6522e0c37d5fc5039d014a
Wilver Martinez <wilver.martinezg@gmail.com>
2017-06-02 15:59:09 -0600

    Agregue pantalla para el detalle de la factura

---
+++ app/src/main/java/gt/umg/viajes/DetalleFacturaActivity.java
@@ -0,0 +1,13 @@
+package gt.umg.viajes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalleFacturaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_factura);
+    }
+}
